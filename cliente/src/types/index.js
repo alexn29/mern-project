6b9f63dbfd45f46488c1bf580634571f11dc0094
unknown <wosorio@hightech-corp.com>
2020-07-12 17:52:10 -0600

    exporting types to manage projects and tasks

---
+++ cliente/src/types/index.js
@@ -0,0 +1,9 @@
+export const PROJECT_FORM = 'PROJECT_FORM';
+export const GET_PROJECTS = 'GET_PROJECTS';
+export const CREATE_PROJECT = 'CREATE_PROJECT';
+export const VALIDATE_FORM = 'VALIDATE_FORM';
+export const CURRENT_PROJECT = 'CURRENT_PROJECT';
+export const DELETE_PROJECT = 'DELETE_PROJECT';
+
+export const TASKS_PROJECT = 'TASKS_PROJECT';
+export const CREATE_TASK = 'CREATE_TASK';
