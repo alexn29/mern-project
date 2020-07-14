3922907b595439ba6e6a8bb54b24901790370a2d
unknown <wosorio@hightech-corp.com>
2020-07-13 21:00:33 -0600

    adding new states and functions to dispatch actions to create, edit and delete tasks

--- cliente/src/context/tasks/taskState.js
+++ cliente/src/context/tasks/taskState.js
@@ -1,21 +1,31 @@
 import React, { useReducer } from 'react';
+import { v4 as uuid } from 'uuid';
+
 import TaskContext from './taskContext';
 import TaskReducer from './taskReducer';
 
 import { 
     TASKS_PROJECT,
-    CREATE_TASK
+    CREATE_TASK,
+    VALIDATE_TASK,
+    DELETE_TASK,
+    STATUS_TASK,
+    CURRENT_TASK,
+    UPDATE_TASK,
+    CLEAN_TASK
 } from '../../types';
 
 const TaskState = props => {
     const initialState = {
         tasks: [
-            { name: 'Elegir Plataforma', status: true, projectId: 1 },
-            { name: 'Elegir Colores', status: false, projectId: 2 },
-            { name: 'Elegir Plataformas de pago', status: true, projectId: 3 },
-            { name: 'Elegir Hosting', status: false, projectId: 4 }
+            { id: 1, name: 'Elegir Plataforma', status: true, projectId: 1 },
+            { id: 2, name: 'Elegir Colores', status: false, projectId: 2 },
+            { id: 3, name: 'Elegir Plataformas de pago', status: true, projectId: 3 },
+            { id: 4, name: 'Elegir Hosting', status: false, projectId: 4 }
         ],
-        tasksProject: null
+        tasksProject: null,
+        errorTask: false,
+        selectedTask: null
     }
 
     const [state, dispatch] = useReducer(TaskReducer, initialState);
@@ -30,9 +40,50 @@ const TaskState = props => {
 
     // crear tarea
     const createTask = task => {
+        task.id = uuid();
         dispatch({
             type: CREATE_TASK,
             payload: task
+        })
+    }
+
+    const validateTask = () => {
+        dispatch({
+            type: VALIDATE_TASK
+        })
+    }
+
+    const deleteTaskByID = id => {
+        dispatch({
+            type: DELETE_TASK,
+            payload: id
+        })
+    }
+
+    const changeStatus = task => {
+        dispatch({
+            type: STATUS_TASK,
+            payload: task
+        })
+    }
+
+    const saveCurrentTask = task => {
+        dispatch({
+            type: CURRENT_TASK,
+            payload: task
+        })
+    }
+
+    const updateTask = task => {
+        dispatch({
+            type: UPDATE_TASK,
+            payload: task
+        })
+    }
+
+    const cleanTask = () => {
+        dispatch({
+            type: CLEAN_TASK
         })
     }
 
@@ -41,8 +92,16 @@ const TaskState = props => {
             value={{
                 tasks: state.tasks,
                 tasksProject: state.tasksProject,
+                errorTask: state.errorTask,
+                selectedTask: state.selectedTask,
                 getTasksByProjectID,
-                createTask
+                createTask,
+                validateTask,
+                deleteTaskByID,
+                changeStatus,
+                saveCurrentTask,
+                updateTask,
+                cleanTask
             }}
         >
             { props.children }
