fa7b9dad7515488a4a4dd1fb804b5a686796dcfc
unknown <wosorio@hightech-corp.com>
2020-07-13 21:01:52 -0600

    adding new actions to create, edit and delete tasks

--- cliente/src/context/tasks/taskReducer.js
+++ cliente/src/context/tasks/taskReducer.js
@@ -1,6 +1,12 @@
 import { 
     TASKS_PROJECT,
-    CREATE_TASK
+    CREATE_TASK,
+    VALIDATE_TASK,
+    DELETE_TASK,
+    STATUS_TASK,
+    CURRENT_TASK,
+    UPDATE_TASK,
+    CLEAN_TASK
 } from "../../types";
 
 export default (state, action) => {
@@ -11,10 +17,43 @@ export default (state, action) => {
                 ...state,
                 tasksProject: state.tasks.filter(task => task.projectId === action.payload)
             }
+
         case CREATE_TASK:
             return {
                 ...state,
-                tasks: [...state.tasks, action.payload]
+                tasks: [...state.tasks, action.payload],
+                errorTask: false
+            }
+        
+        case VALIDATE_TASK:
+            return {
+                ...state,
+                errorTask: true
+            }
+        
+        case DELETE_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.filter(task => task.id !== action.payload)
+            }
+        
+        case UPDATE_TASK:
+        case STATUS_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.map(task => task.id === action.payload.id ? action.payload : task)
+            }
+        
+        case CURRENT_TASK:
+            return {
+                ...state,
+                selectedTask: action.payload
+            }
+
+        case CLEAN_TASK:
+            return {
+                ...state,
+                selectedTask: null
             }
 
         default:
